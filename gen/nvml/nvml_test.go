7bca10611bc6c78441022937f59f02072ccfed14
Kevin Klues <kklues@nvidia.com>
2019-12-02 11:42:36 +0000

    Add tests for NVML Unit bindings
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- gen/nvml/nvml_test.go
+++ gen/nvml/nvml_test.go
@@ -88,3 +88,144 @@ func TestSystem(t *testing.T) {
 		}
 	}
 }
+
+func TestUnit(t *testing.T) {
+	Init()
+	defer Shutdown()
+
+	unitCount, ret := UnitGetCount()
+	if ret != SUCCESS {
+		t.Errorf("UnitGetCount: %v", ret)
+	} else {
+		t.Logf("UnitGetCount: %v", ret)
+		t.Logf("  count: %v", unitCount)
+	}
+
+	if unitCount == 0 {
+		t.Skip("Skipping test with no Units.")
+	}
+
+	unit, ret := UnitGetHandleByIndex(0)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetHandleByIndex: %v", ret)
+	} else {
+		t.Logf("UnitGetHandleByIndex: %v", ret)
+		t.Logf("  unit: %v", unit)
+	}
+
+	info, ret := UnitGetUnitInfo(unit)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetUnitInfo: %v", ret)
+	} else {
+		t.Logf("UnitGetUnitInfo: %v", ret)
+		t.Logf("  info: %v", info)
+	}
+
+	info, ret = unit.GetUnitInfo()
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetUnitInfo: %v", ret)
+	} else {
+		t.Logf("Unit.GetUnitInfo: %v", ret)
+		t.Logf("  info: %v", info)
+	}
+
+	state, ret := UnitGetLedState(unit)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetLedState: %v", ret)
+	} else {
+		t.Logf("UnitGetLedState: %v", ret)
+		t.Logf("  state: %v", state)
+	}
+
+	state, ret = unit.GetLedState()
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetLedState: %v", ret)
+	} else {
+		t.Logf("Unit.GetLedState: %v", ret)
+		t.Logf("  state: %v", state)
+	}
+
+	psu, ret := UnitGetPsuInfo(unit)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetPsuInfo: %v", ret)
+	} else {
+		t.Logf("UnitGetPsuInfo: %v", ret)
+		t.Logf("  psu: %v", psu)
+	}
+
+	psu, ret = unit.GetPsuInfo()
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetPsuInfo: %v", ret)
+	} else {
+		t.Logf("Unit.GetPsuInfo: %v", ret)
+		t.Logf("  psu: %v", psu)
+	}
+
+	temp, ret := UnitGetTemperature(unit, 0)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetTemperature: %v", ret)
+	} else {
+		t.Logf("UnitGetTemperature: %v", ret)
+		t.Logf("  temp: %v", temp)
+	}
+
+	temp, ret = unit.GetTemperature(0)
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetTemperature: %v", ret)
+	} else {
+		t.Logf("Unit.GetTemperature: %v", ret)
+		t.Logf("  temp: %v", temp)
+	}
+
+	speed, ret := UnitGetFanSpeedInfo(unit)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetFanSpeedInfo: %v", ret)
+	} else {
+		t.Logf("UnitGetFanSpeedInfo: %v", ret)
+		t.Logf("  speed: %v", speed)
+	}
+
+	speed, ret = unit.GetFanSpeedInfo()
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetFanSpeedInfo: %v", ret)
+	} else {
+		t.Logf("Unit.GetFanSpeedInfo: %v", ret)
+		t.Logf("  speed: %v", speed)
+	}
+
+	devices, ret := UnitGetDevices(unit)
+	if ret != SUCCESS {
+		t.Errorf("UnitGetDevices: %v", ret)
+	} else {
+		t.Logf("UnitGetDevices: %v", ret)
+		t.Logf("  count: %v", len(devices))
+		for i, device := range devices {
+			t.Logf("  device[%v]: %v", i, device)
+		}
+	}
+
+	devices, ret = unit.GetDevices()
+	if ret != SUCCESS {
+		t.Errorf("Unit.GetDevices: %v", ret)
+	} else {
+		t.Logf("Unit.GetDevices: %v", ret)
+		t.Logf("  count: %v", len(devices))
+		for i, device := range devices {
+			t.Logf("  device[%v]: %v", i, device)
+		}
+	}
+
+	ret = UnitSetLedState(unit, 0)
+	if ret != SUCCESS {
+		t.Errorf("UnitSetLedState: %v", ret)
+	} else {
+		t.Logf("UnitSetLedState: %v", ret)
+	}
+
+	ret = unit.SetLedState(0)
+	if ret != SUCCESS {
+		t.Errorf("Unit.SetLedState: %v", ret)
+	} else {
+		t.Logf("Unit.SetLedState: %v", ret)
+	}
+}
