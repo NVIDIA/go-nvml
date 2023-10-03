b8d34ba5dc71c7b5a261bbdfdec63fe337fac2a5
braydonk <braydonk@google.com>
2023-10-03 03:32:50 +0000

    gen/nvml: add --export-dynamic linker flag
    Signed-off-by: braydonk <braydonk@google.com>

--- pkg/nvml/nvml.go
+++ pkg/nvml/nvml.go
@@ -18,7 +18,7 @@
 package nvml
 
 /*
-#cgo LDFLAGS: -Wl,--unresolved-symbols=ignore-in-object-files
+#cgo LDFLAGS: -Wl,--export-dynamic -Wl,--unresolved-symbols=ignore-in-object-files
 #cgo CFLAGS: -DNVML_NO_UNVERSIONED_FUNC_DEFS=1
 #include "nvml.h"
 #include <stdlib.h>
